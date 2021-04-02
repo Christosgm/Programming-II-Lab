c52d0869a6b93228ae2490e2a9b3f074b2edd058
Christos Georgiou Mousses <x.georgiou.mousses@gmail.com>
2021-04-02 21:04:07 +0300

    Coord input, input validation, 2d array implementation

--- README.md
+++ README.md
@@ -3,10 +3,10 @@ Welcome to the Programming II Lab repository!
 Here you will find the source code of the game tic-tac-toe we are implementing!
 
 # Structure
-- [ ] Menu
-- [ ] Input System
-- [ ] Input Validation
-- [ ] Representation
+- [x] Menu
+- [x] Input System (Basic)
+- [x] Input Validation (Basic)
+- [x] Representation
 - [ ] Game Implementation
 - [ ] Save System
 - [ ] Load System
--- triliza.c
+++ triliza.c
@@ -3,16 +3,25 @@
 
 int menu();
 void clear_stdin();
-void print_board();
+void init_board(char board[][3]);
+void print_board(char board[][3]);
+int input(int * row, int * col);
+int is_valid_input(char in[4]);
 
 int main()
 {
-	int choice = menu();
-	if(choice == 2 || choice == 3)
+	int choice = menu();//Get choice from player
+	if(choice == 2 || choice == 3)//If choice == load or exit
 	{
 		exit(1);
 	}
-	print_board();
+	char board[3][3];
+	init_board(board);//Fill board with ' '
+	int row, col;//Row and column to put symbol
+	print_board(board);
+	input(&row, &col);//At the memory address of row and col take the input
+	board[row - 1][col - 1] = 'X';//Put symbol at the [row, col]
+	print_board(board);
 	return 0;
 }
 
@@ -36,29 +45,73 @@ int menu()
 
 void clear_stdin()
 {
-	char c;
-	while((c = getchar()) != '\n' && c != EOF);
+	fflush(stdin);
 }
 
-void print_board()
+void print_board(char board[][3])
 {
 	int i, j;
-    printf("    1   2   3   \n");
-    printf("  /-----------\\\n");
-    for(i = 0; i < 3; i++)
-    {
-        printf("%d | ", i + 1);
-        for(j = 0; j < 3; j++)
-        {
-            printf("%d | ", i + j);
-        }
-        if(i != 2)
-        {
-            printf("\n  |-----------|\n");
-        }
-        else
-        {
-            printf("\n  \\-----------/\n");
-        }
-    }
+	printf("    1   2   3   \n");
+	printf("  /-----------\\\n");//Starting divider
+	for(i = 0; i < 3; i++)//For every line
+	{
+		printf("%d | ", i + 1);//Print line number
+		for(j = 0; j < 3; j++)//For every column
+		{
+			printf("%c | ", board[i][j]);//Print content then divider
+		}
+		if(i != 2)//For the first two loops
+		{
+			printf("\n  |-----------|\n");//Print middle divider
+		}
+	}
+	printf("\n  \\-----------/\n");//When done print ending divider
+}
+
+void init_board(char board[][3])
+{
+	int i, j;
+	//For every line
+	for(i = 0; i < 3; i++)
+	{
+		//For every column
+		for(j = 0; j < 3; j++)
+		{
+			board[i][j] = ' ';
+		}
+	}
+}
+
+int input(int * row, int * col)
+{
+	int res;
+	char in[4];
+	while(1)
+	{
+		printf("Insert row and column: ");
+		fgets(in, 4, stdin);//Get 3 characters from stdin (number space number)
+		clear_stdin();//Clear stdin (\n is left in stdin)
+		res = is_valid_input(in);//Check for validity
+		if(res == 0)//If not valid
+		{
+			printf("Input must be '[1, 3] [' '] [1, 3]'\n:>");
+			continue;
+		}
+		if(res == 1)//If valid
+		{
+			*row = in[0] - '0';
+			*col = in[2] - '0';
+			return 1;
+		}
+	}
+}
+
+int is_valid_input(char in[4])
+{
+	//String must be of the form ([1-3], space, [1-3], \0)
+	if((in[0] >= '1' && in[0] <= '3') && in[1] == ' ' && (in[2] >= '1' && in[2] <= '3') && in[3] == '\0')
+	{
+		return 1;
+	}
+	return 0;
 }
